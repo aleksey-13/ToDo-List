e08c369caee0e4f783173692a4bc5e6a99211897
Menge <alekseymagadan@gmail.com>
2019-10-04 16:14:22 +0300

    Create a project React structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('123')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
+
+const App = () => {
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel />
+            <TodoList />
+        </div>
+    );
+}
+
+//const el = React.createElement('h1', null, 'La la la');
+
+ReactDOM.render(<App />, document.getElementById('root'));
